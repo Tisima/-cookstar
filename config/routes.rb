af6cb7f9be2e98b52b8ec2e8861647a17647acb9
Tisima <zerosan487@gmail.com>
2022-08-12 13:22:59 +0000

    [AddLogin]ゲスト、会員ログイン機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,16 @@ devise_for :users, skip: [:passwords], controllers: {
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+root "homes#top"
+get 'homes/about'
+post '/homes/guest_sign_in', to: 'homes#guest_sign_in'
+
+scope module: :user do
+  resources :recipes, only: [:show, :index]
+  get 'relationships/followings'
+  get 'relationships/followers'
+end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 end
